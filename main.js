6df5102163dd0271c8e90f713af3469cc13bba5f
Arthur <addel-3@web.de>
2025-11-23 16:00:55 +0100

    basic plot working

--- main.js
+++ main.js
@@ -15,34 +15,44 @@ async function loadFile() {
   const response = await fetch("cars.csv");
   const content = await response.text();   //  content as a string
 
-  console.log(content);
+  // console.log(content);
 
-  const xSize = 500; 
+  const xSize = 600;
   const ySize = 500;
   const margin = 40;
-  const xMax = xSize - margin*2;
-  const yMax = ySize - margin*2;
+  const xMax = xSize - margin * 2;
+  const yMax = 100;
 
   // Create Random Points
   const data = [];
   const cars = CSVToArray(content);
-  const numPoints = cars.length;
+  const numPoints = cars.length / 1;
 
+  const type_index = 1;
   const hp_index = 8;
   const mpg_index = 9
 
-  const yScale = 0.1;
+  const yScale = 5;
 
   for (let i = 1; i < numPoints; i++) {
-    data.push([cars[i][hp_index], cars[i][mpg_index]]);
-    console.log([cars[i][hp_index], cars[i][mpg_index]]);
+    data.push([cars[i][hp_index], cars[i][mpg_index], cars[i][type_index]]);
+    // console.log([cars[i][hp_index], cars[i][mpg_index]]);
   }
 
+  const car_colors = new Map([
+    ["Sedan", "Blue"],
+    ["SUV", "Gray"],
+    ["Sports Car", "Red"],
+    ["Wagon", "Purple"],
+    ["Minivan", "Pink"]
+  ])
+
+  console.log(new Set(cars.map(car => car[type_index])))
   // Append SVG Object to the Page
   const svg = d3.select("#myPlot")
     .append("svg")
     .append("g")
-    .attr("transform","translate(" + margin + "," + margin + ")");
+    .attr("transform", "translate(" + margin + "," + margin + ")");
 
   // X Axis
   const x = d3.scaleLinear()
@@ -55,8 +65,8 @@ async function loadFile() {
 
   // Y Axis
   const y = d3.scaleLinear()
-    .domain([0, yScale * yMax])
-    .range([ yScale * yMax, 0]);
+    .domain([0, yMax])
+    .range([yScale * yMax, 0]);
 
   svg.append("g")
     .call(d3.axisLeft(y));
@@ -66,99 +76,128 @@ async function loadFile() {
     .selectAll("dot")
     .data(data).enter()
     .append("circle")
-    .attr("cx", function (d) { return d[0] } )
-    .attr("cy", function (d) { return yScale * (yMax - d[1]) } )
+    .attr("cx", function (d) { return d[0] })
+    .attr("cy", function (d) { return yScale * (yMax - d[1]) })
     .attr("r", 3)
-    .style("fill", "Red");
-  
+    .style("fill", function (d) { return car_colors.get(d[2]) });
+
+
+  const legend = svg.append("g")
+    .attr("class", "legend")
+    .attr("transform", "translate(300, -10)");
+  const legendData = Array.from(car_colors, ([label, color]) => ({ label, color }));
+
+  // Legend circles
+  legend.selectAll(".legend-dot")
+    .data(legendData)
+    .enter()
+    .append("circle")
+    .attr("class", "legend-dot")
+    .attr("cx", 100)
+    .attr("cy", (d, i) => 100 + i * 25)
+    .attr("r", 7)
+    .style("fill", d => d.color);
+
+  // Legend labels
+  legend.selectAll(".legend-label")
+    .data(legendData)
+    .enter()
+    .append("text")
+    .attr("class", "legend-label")
+    .attr("x", 120)
+    .attr("y", (d, i) => 100 + i * 25)
+    .text(d => d.label)
+    .attr("text-anchor", "start")
+    .style("alignment-baseline", "middle");
 }
+
 
 
 // ref: http://stackoverflow.com/a/1293163/2343
 // This will parse a delimited string into an array of
 // arrays. The default delimiter is the comma, but this
 // can be overriden in the second argument.
-function CSVToArray( strData, strDelimiter ){
-    // Check to see if the delimiter is defined. If not,
-    // then default to comma.
-    strDelimiter = (strDelimiter || ",");
+function CSVToArray(strData, strDelimiter) {
+  // Check to see if the delimiter is defined. If not,
+  // then default to comma.
+  strDelimiter = (strDelimiter || ",");
 
-    // Create a regular expression to parse the CSV values.
-    var objPattern = new RegExp(
-        (
-            // Delimiters.
-            "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+  // Create a regular expression to parse the CSV values.
+  var objPattern = new RegExp(
+    (
+      // Delimiters.
+      "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
 
-            // Quoted fields.
-            "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+      // Quoted fields.
+      "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
 
-            // Standard fields.
-            "([^\"\\" + strDelimiter + "\\r\\n]*))"
-        ),
-        "gi"
-        );
-
-
-    // Create an array to hold our data. Give the array
-    // a default empty first row.
-    var arrData = [[]];
-
-    // Create an array to hold our individual pattern
-    // matching groups.
-    var arrMatches = null;
+      // Standard fields.
+      "([^\"\\" + strDelimiter + "\\r\\n]*))"
+    ),
+    "gi"
+  );
 
 
-    // Keep looping over the regular expression matches
-    // until we can no longer find a match.
-    while (arrMatches = objPattern.exec( strData )){
+  // Create an array to hold our data. Give the array
+  // a default empty first row.
+  var arrData = [[]];
 
-        // Get the delimiter that was found.
-        var strMatchedDelimiter = arrMatches[ 1 ];
-
-        // Check to see if the given delimiter has a length
-        // (is not the start of string) and if it matches
-        // field delimiter. If id does not, then we know
-        // that this delimiter is a row delimiter.
-        if (
-            strMatchedDelimiter.length &&
-            strMatchedDelimiter !== strDelimiter
-            ){
-
-            // Since we have reached a new row of data,
-            // add an empty row to our data array.
-            arrData.push( [] );
-
-        }
-
-        var strMatchedValue;
-
-        // Now that we have our delimiter out of the way,
-        // let's check to see which kind of value we
-        // captured (quoted or unquoted).
-        if (arrMatches[ 2 ]){
-
-            // We found a quoted value. When we capture
-            // this value, unescape any double quotes.
-            strMatchedValue = arrMatches[ 2 ].replace(
-                new RegExp( "\"\"", "g" ),
-                "\""
-                );
-
-        } else {
-
-            // We found a non-quoted value.
-            strMatchedValue = arrMatches[ 3 ];
-
-        }
+  // Create an array to hold our individual pattern
+  // matching groups.
+  var arrMatches = null;
 
 
-        // Now that we have our value string, let's add
-        // it to the data array.
-        arrData[ arrData.length - 1 ].push( strMatchedValue );
+  // Keep looping over the regular expression matches
+  // until we can no longer find a match.
+  while (arrMatches = objPattern.exec(strData)) {
+
+    // Get the delimiter that was found.
+    var strMatchedDelimiter = arrMatches[1];
+
+    // Check to see if the given delimiter has a length
+    // (is not the start of string) and if it matches
+    // field delimiter. If id does not, then we know
+    // that this delimiter is a row delimiter.
+    if (
+      strMatchedDelimiter.length &&
+      strMatchedDelimiter !== strDelimiter
+    ) {
+
+      // Since we have reached a new row of data,
+      // add an empty row to our data array.
+      arrData.push([]);
+
     }
 
-    // Return the parsed data.
-    return( arrData );
+    var strMatchedValue;
+
+    // Now that we have our delimiter out of the way,
+    // let's check to see which kind of value we
+    // captured (quoted or unquoted).
+    if (arrMatches[2]) {
+
+      // We found a quoted value. When we capture
+      // this value, unescape any double quotes.
+      strMatchedValue = arrMatches[2].replace(
+        new RegExp("\"\"", "g"),
+        "\""
+      );
+
+    } else {
+
+      // We found a non-quoted value.
+      strMatchedValue = arrMatches[3];
+
+    }
+
+
+    // Now that we have our value string, let's add
+    // it to the data array.
+    arrData[arrData.length - 1].push(strMatchedValue);
+  }
+
+  // Return the parsed data.
+  return (arrData);
 }
 
 
