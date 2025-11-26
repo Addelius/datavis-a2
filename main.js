da34da2d227a0bf493c1e74e30472d23251fac02
Arthur <addel-3@web.de>
2025-11-26 10:30:15 +0100

    tooltip working

--- main.js
+++ main.js
@@ -28,14 +28,20 @@ async function loadFile() {
   const cars = CSVToArray(content);
   const numPoints = cars.length / 1;
 
-  const type_index = 1;
-  const hp_index = 8;
-  const mpg_index = 9
+  const selected_attributes = ["Horsepower(HP)", "City Miles Per Gallon", "Type", "AWD", "Retail Price", "Engine Size (l)", "Cyl"];
+
+  const attribute_indizes = selected_attributes.map(attribute => cars[0].indexOf(attribute));
+  const type_index = cars[0].indexOf("Type");
 
   const yScale = 5;
 
   for (let i = 1; i < numPoints; i++) {
-    data.push([cars[i][hp_index], cars[i][mpg_index], cars[i][type_index]]);
+    const dataset = attribute_indizes.map(index => cars[i][index]);
+    data.push(dataset);
+    if (i % 10 == 0) {
+      // console.log(dataset)
+    }
+    // data.push([cars[i][hp_index], cars[i][mpg_index], cars[i][type_index]]);
     // console.log([cars[i][hp_index], cars[i][mpg_index]]);
   }
 
@@ -47,17 +53,17 @@ async function loadFile() {
     ["Minivan", "Pink"]
   ])
 
-  console.log(new Set(cars.map(car => car[type_index])))
+  // console.log(new Set(cars.map(car => car[type_index])))
   // Append SVG Object to the Page
   const svg = d3.select("#myPlot")
     .append("svg")
     .append("g")
-    .attr("transform", "translate(" + margin + "," + margin + ")");
+    .attr("transform", "translate(" + 2 * margin + "," + margin + ")");
 
   // X Axis
   const x = d3.scaleLinear()
     .domain([0, xMax])
-    .range([0, xMax]);
+    .range([0, 2 * xMax]);
 
   svg.append("g")
     .attr("transform", "translate(0," + yScale * yMax + ")")
@@ -71,22 +77,54 @@ async function loadFile() {
   svg.append("g")
     .call(d3.axisLeft(y));
 
+  // X Axis label
+  svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "middle")
+    .attr("x", xMax / 2)
+    .attr("y", yScale * yMax + 35)
+    .text("Horsepower");
+
+  // Y Axis label
+  svg.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -(yScale * yMax) / 2)
+    .attr("y", -35)
+    .text("City Miles Per Gallon");
+
   // Dots
+  const AWDtoSymbol = {
+    "0": d3.symbolCircle,
+    "1": d3.symbolSquare
+  };
+
   svg.append('g')
     .selectAll("dot")
     .data(data).enter()
-    .append("circle")
-    .attr("cx", function (d) { return d[0] })
-    .attr("cy", function (d) { return yScale * (yMax - d[1]) })
-    .attr("r", 3)
-    .style("fill", function (d) { return car_colors.get(d[2]) });
+    .append("path")
+    .attr("transform", d =>
+      `translate(${2 * d[0]}, ${yScale * (yMax - d[1])})`
+    )
+    .attr("d", d => d3.symbol()
+      .type(AWDtoSymbol[d[3]])
+      .size(40)()
+    )
+    .style("fill", function (d) { return car_colors.get(d[2]) })
+    .style("cursor", "pointer")
+    .on("mouseover", handleDotHover)
+    .on("mousemove", moveTooltip)
+    .on("mouseout", hideTooltip);
 
 
   const legend = svg.append("g")
     .attr("class", "legend")
     .attr("transform", "translate(300, -10)");
   const legendData = Array.from(car_colors, ([label, color]) => ({ label, color }));
-
+  // console.log(legendData);
+  legendData.push({label: "Square: AWD", color: "White"});
+  legendData.push({label: "Circle: no AWD", color: "White"});
   // Legend circles
   legend.selectAll(".legend-dot")
     .data(legendData)
@@ -109,6 +147,47 @@ async function loadFile() {
     .text(d => d.label)
     .attr("text-anchor", "start")
     .style("alignment-baseline", "middle");
+}
+//  const selected_attributes = ["Horsepower(HP)", "City Miles Per Gallon", "Type", "AWD", "Retail Price", "Engine Size (l)", "Cyl"];
+
+function handleDotHover(d, i) {
+  console.log(d)
+  const hp = d[0];
+  const mpg = d[1];
+  const type = d[2];
+  const AWD = Number(d[3]);
+  const price = d[4]
+  const engine_size = d[5];
+  const cyl = d[6];
+
+  const tooltip = document.getElementById("carTooltip");
+
+  tooltip.innerHTML = `
+    <table>
+      <tr><th colspan="2" style="text-align:center">${type}</th></tr>
+      <tr><td><b>Horsepower</b></td><td>${hp}</td></tr>
+      <tr><td><b>City MPG</b></td><td>${mpg}</td></tr>
+      <tr><td><b>AWD</b></td><td>${AWD ? "yes" : "no"}</td></tr>
+      <tr><td><b>Retail Price</b></td><td>${price}</td></tr>
+      <tr><td><b>Engine Size (l)</b></td><td>${engine_size}</td></tr>
+      <tr><td><b>Cylinders</b></td><td>${cyl}</td></tr>
+    </table>
+  `;
+
+  tooltip.style.display = "block";
+}
+
+function moveTooltip(d, i) {
+  const tooltip = document.getElementById("carTooltip");
+  const offset = 15;
+
+  tooltip.style.left = (event.pageX + offset) + "px";
+  tooltip.style.top = (event.pageY + offset) + "px";
+}
+
+function hideTooltip() {
+  const tooltip = document.getElementById("carTooltip");
+  tooltip.style.display = "none";
 }
 
 
